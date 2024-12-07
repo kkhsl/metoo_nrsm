945b17ac881e7ff123aaa3503c38423485fcf04b
15838152042@189.cn <15838152042@189.cn>
2024-12-07 18:48:40 +0800

    metoo/ping

--- src/main/java/com/metoo/nrsm/core/utils/unbound/strategy/impl/PrivateAddressUpdateStrategy.java
+++ src/main/java/com/metoo/nrsm/core/utils/unbound/strategy/impl/PrivateAddressUpdateStrategy.java
@@ -30,7 +30,7 @@ public class PrivateAddressUpdateStrategy implements ConfigUpdateStrategy {
             // 判断该行是否包含 "private-address: ::/0"（无论是否有注释）
             if (trimmedLine.replaceFirst("^#\\s*", "").startsWith("private-address: ::/0")) {
                 // 获取当前行的缩进部分
-                String value = "private-address: ::/0";
+                String value = trimmedLine.replaceFirst("^#\\s*", "");
                 if (privateAddress) {
                     // 如果 privateAddress 为 true 且该行是注释掉的，则去掉注释
 
