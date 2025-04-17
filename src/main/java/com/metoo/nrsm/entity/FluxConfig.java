77ae5fe928177845e753d0d52cd3822c7fddf5ed
xushiliang <471343737@qq.com>
2025-04-17 16:58:51 +0800

    流量配置新增v3参数

--- src/main/java/com/metoo/nrsm/entity/FluxConfig.java
+++ src/main/java/com/metoo/nrsm/entity/FluxConfig.java
@@ -2,6 +2,7 @@ package com.metoo.nrsm.entity;
 
 import com.metoo.nrsm.core.domain.IdEntity;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,7 @@ public class FluxConfig extends IdEntity {
 
     private String community;
 
+    @ApiModelProperty("SNMP版本")
     private String version;
 
     private String ipv4Oid;
@@ -39,5 +41,23 @@ public class FluxConfig extends IdEntity {
     private List<List<String>> ipv6Oids = new ArrayList<>();
 
     private Integer update;
+
+    // snmpv3
+    // 安全模型
+    // noAuthNoPriv：无认证和加密，最不安全的模式
+    // authNoPriv：仅认证，但不加密
+    // authPriv：认证并加密，是最安全的模式
+    @ApiModelProperty("用户名")
+    private String securityName;
+    @ApiModelProperty("安全等级 1 2 3")
+    private Integer securityLevel;
+    @ApiModelProperty("认证协议 MD5/SHA")
+    private String authProtocol;
+    @ApiModelProperty("认证密码")
+    private String authPassword;
+    @ApiModelProperty("加密协议 DES/AES")
+    private String privProtocol;
+    @ApiModelProperty("加密密码")
+    private String privPassword;
 
 }
